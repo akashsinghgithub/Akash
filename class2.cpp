bca4feb8a98bec97583407ace3ef5b736a96af0b
unknown <2200520319002@ietlucknow.ac.in>
2023-09-23 20:10:41 +0530

    first commit

---
+++ class2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    a = a * a;
+    cout << " Sqare of area : " << a << endl ;
+
+    return 0;
+
+}
